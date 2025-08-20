7a058ac2496d3fb81f59dfbb72098c2a900e2e38
Uttam Mahata <uttambav@gmail.com>
2025-08-20 11:14:31 +0530

    chore: Remove unused SQL initialization script and development helper script

--- server/init.sql
+++
@@ -1,8 +0,0 @@
--- Initial setup script for MySQL
-CREATE DATABASE IF NOT EXISTS icrogen;
-USE icrogen;
-
--- Create user if not exists
-CREATE USER IF NOT EXISTS 'icrogen_user'@'%' IDENTIFIED BY 'icrogen_pass';
-GRANT ALL PRIVILEGES ON icrogen.* TO 'icrogen_user'@'%';
-FLUSH PRIVILEGES;
